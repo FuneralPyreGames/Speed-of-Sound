eb67ba5e23c3a52a3c3eda773b8711c212e509b7
Liam Palmer <88364745+FuneralPyreGames@users.noreply.github.com>
2022-01-09 23:39:08 -0500

    ohso

--- Assets/Scripts/MainMenu/MenuTweening.cs
+++ Assets/Scripts/MainMenu/MenuTweening.cs
@@ -33,7 +33,7 @@ public class MenuTweening : MonoBehaviour
     {
         LeanTween.moveLocalX(mainMenuLeft, -350, .35f);
         LeanTween.moveLocalX(mainMenuRight, 175, .35f);
-        LeanTween.moveLocalY(mainMenuTop, 250, .35f);
+        LeanTween.moveLocalY(mainMenuTop, 500, .35f);
         LeanTween.moveLocalY(helpOverlay, 0, .35f);
     }
     public void TweenHelpToMainMenu()
@@ -47,7 +47,7 @@ public class MenuTweening : MonoBehaviour
     {
         LeanTween.moveLocalX(mainMenuLeft, -350, .35f);
         LeanTween.moveLocalX(mainMenuRight, 175, .35f);
-        LeanTween.moveLocalY(mainMenuTop, 250, .35f);
+        LeanTween.moveLocalY(mainMenuTop, 500, .35f);
         LeanTween.moveLocalX(settingsOverlay, 0, .35f);
     }
     public void TweenSettingsToMainMenu()
@@ -61,7 +61,7 @@ public class MenuTweening : MonoBehaviour
     {
         LeanTween.moveLocalX(mainMenuLeft, -350, .35f);
         LeanTween.moveLocalX(mainMenuRight, 175, .35f);
-        LeanTween.moveLocalY(mainMenuTop, 250, .35f);
+        LeanTween.moveLocalY(mainMenuTop, 500, .35f);
         LeanTween.moveLocalY(creditsOverlay, 0, .35f);
     }
     public void TweenCreditsToMainMenu()
