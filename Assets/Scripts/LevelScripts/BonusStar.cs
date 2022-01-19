7ddc7a70aa5f22023502b71514c0d062e257de71
FuneralPyreGames <liambobpalmer@gmail.com>
2022-01-18 23:07:16 -0500

    bonus stars yeah

--- Assets/Scripts/LevelScripts/BonusStar.cs
+++ Assets/Scripts/LevelScripts/BonusStar.cs
@@ -6,18 +6,19 @@ using Photon.Pun;
 
 public class BonusStar : MonoBehaviour
 {
-    private int bonusStarNumber;
-    private void OnTriggerEnter(Collider other) 
+    public int bonusStarNumber;
+    private void OnTriggerEnter(Collider other)
     {
         //To begin, the bonus star script checks to see if the master client got the star, and if it doesn't, it uses a rpc call to ensure the master client does
         var playerController = other.gameObject.GetComponent<PlayerController>();
-        if (PhotonNetwork.IsMasterClient)
+        switch (PhotonNetwork.IsMasterClient)
         {
-            playerController.SaveBonusStar(bonusStarNumber);
-        }
-        else if (!PhotonNetwork.IsMasterClient)
-        {
-            playerController.RPCToMainPlayer(bonusStarNumber);
+            case true:
+                playerController.SaveBonusStar(bonusStarNumber);
+                break;
+            case false:
+                playerController.RPCToMainPlayer(bonusStarNumber);
+                break;
         }
     }
 }
